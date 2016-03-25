0a3d318674e3f86d3827ae65a007c84c237355d9
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-03-25 17:00:42 +0700

    The basic idea kind of works

--- letter-to-a4.sh
+++ letter-to-a4.sh
@@ -6,18 +6,29 @@ main () {
   for style in $(styles) ; do
     for page_num in $(page_numbers) ; do
       local source=$(source_file $style $page_num)
-      local target=$(target_file $style $page_num)
+      local target=$(target_name $style 'A4')
 
-      convert ${source} ${target}
+      convert_page ${source} ${target} ${page_num}
     done
+    collate ${target}
   done
 }
 
-convert () {
-  local opts="-sDEVICE=pdfwrite -sPAPERSIZE=a4 -dFIXEDMEDIA -dPDFFitPage -dCompatibilityLevel=1.4"
+convert_page () {
+  local source=${1}
+  local target_name=${2}
+  local page_num=${3}
 
-  echo "gs $opts -o $target $source"
-  gs $opts -o ${target} ${source}
+  local target_dir=${target_name}/pages
+  local target_file=${target_name}/pages/Chart-$(pad ${page_num}).pdf
+
+  local opts="-q -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dFIXEDMEDIA -dPDFFitPage -dCompatibilityLevel=1.4"
+
+  echo "Convert: $source -> $target_file" >&2
+  echo "  gs $opts -o $target_file $source" >&2
+
+  mkdir -p ${target_dir}
+  gs $opts -o ${target_file} ${source}
 }
 
 source_file () {
@@ -26,13 +37,32 @@ source_file () {
   echo "source/Mag7-$style/Chart-$num.pdf"
 }
 
+target_name () {
+  local style=$1
+  local size=$2
+  local dir="target/Mag7/$style/$size"
+
+  mkdir -p ${dir}
+  echo ${dir}
+}
+
+collate () {
+  local target_dir=${1}/pages
+  local target_file=${1}-All.pdf
+  local opts="-q -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dFIXEDMEDIA -dPDFFitPage -dCompatibilityLevel=1.4"
+
+  echo "Collate: $target_name" >&2
+  echo "  gs $opts -o $target_file $target_dir/*.pdf" >&2
+  gs $opts -o ${target_file} ${target_dir}/*.pdf
+}
+
 target_file () {
+  local dirname=$(target_dir $@)
   local style=$1
   local num=$2
   local filename="Chart-$(pad $num).pdf"
   local dirname="target/Mag7-$style-A4"
 
-  mkdir -p ${dirname}
   echo ${dirname}/${filename}
 }
 
