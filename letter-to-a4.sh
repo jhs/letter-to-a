3940cd22860cfd49a5b50facc26c5e46a3a0c775
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-03-25 20:19:06 +0700

    The build kind of works

--- letter-to-a4.sh
+++ letter-to-a4.sh
@@ -8,14 +8,17 @@ source_dir=${root}/source
 main () {
   prep_source_files
 
-  for style in $(styles) ; do
-    for page_num in $(page_numbers) ; do
-      local source=$(source_file $style $page_num)
-      local target=$(target_name $style 'A4')
+  for size in A4 ; do
+    for style in $(styles) ; do
+      for page_num in $(page_numbers) ; do
+        local source=$(source_file $style $page_num)
+        local target=$(target_name $style $size)
 
-      convert_page ${source} ${target} ${page_num}
+        convert_page ${source} ${target} ${page_num}
+      done
+
+      collate $size $style
     done
-    collate ${target}
   done
 }
 
@@ -39,36 +42,30 @@ convert_page () {
 source_file () {
   local style=$1
   local num=$2
-  echo "source/Mag7-$style/Chart-$num.pdf"
+  echo "$root/source/Mag7-$style/Chart-$num.pdf"
 }
 
 target_name () {
   local style=$1
   local size=$2
-  local dir="target/Mag7/$style/$size"
+  local dir="$root/target/Mag7/$style/$size"
 
   mkdir -p ${dir}
   echo ${dir}
 }
 
 collate () {
-  local target_dir=${1}/pages
-  local target_file=${1}-All.pdf
+  local size=$1
+  local style=$2
+  local target_name=$(target_name $style $size)
+  local source_dir=${target_name}/pages
+  local target_file="${target_name}/Mag7-$style-$size-All.pdf"
+
   local opts="-q -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dFIXEDMEDIA -dPDFFitPage -dCompatibilityLevel=1.4"
 
   echo "Collate: $target_name" >&2
-  echo "  gs $opts -o $target_file $target_dir/*.pdf" >&2
-  gs $opts -o ${target_file} ${target_dir}/*.pdf
-}
-
-target_file () {
-  local dirname=$(target_dir $@)
-  local style=$1
-  local num=$2
-  local filename="Chart-$(pad $num).pdf"
-  local dirname="target/Mag7-$style-A4"
-
-  echo ${dirname}/${filename}
+  echo "  gs $opts -o $target_file $source_dir/*.pdf" >&2
+  gs $opts -o ${target_file} ${source_dir}/*.pdf
 }
 
 pad () {
